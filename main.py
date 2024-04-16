16870555640fb657aec6a0a963bd0d5fc5e20e5c
arturlech <163434721+arturlech@users.noreply.github.com>
2024-04-16 17:02:50 +0200

    wip: read code refactor to function

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ users: list = [
     {"name": "Adrian", "surname": "Dobrzański", "posts": 1, }
 
 ]
-print("Informacje o twoich znajomych: ")
-for user in users:
-    print(f'\tTwój znajmomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
+def read_friends(users: list)->None:
+    print("Informacje o twoich znajomych: ")
+    for user in users:
+        print(f'\tTwój znajmomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
+
+read_friends(users)
